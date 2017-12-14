e25eb55a8fa3122efeea32a761224400ad61755c
Max Shalom <max_shalom121@flatbush.org>
2017-12-14 11:09:40 -0500

    Add 1-hello-world.py

---
+++ Basics/1-hello-word.py
@@ -0,0 +1,12 @@
+# Tutorial: https://www.learnpython.org/en/Hello%2C_World%21
+
+# Hello World
+print("Hello World!")
+
+# Indentation: In Python is indented with 4 spaces instead of having curly braces, like some other languages.
+x = 2
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+else:
+    print("x is not 1.")
